f8a92d5361f89c2102b0c0143eef20c36ebb6983
zkirtaem <zkirtaem@meat.meat>
2016-06-20 02:12:05 +0200

    Now it's possible to switch active iptables rules off.

--- block.sh
+++ block.sh
@@ -4,15 +4,36 @@ fi
 
 counter=0;
 dialogstring=''
+iptables_rules=`sudo iptables --list INPUT | tail -n +3 | awk '{print $4}'`
 
 for filename in `ls serverdata`; do
    (( counter++ ))
-   dialogstring=$dialogstring" "$counter" "$filename" off"
+   dialogstring=$dialogstring" "$counter" "$filename
+
+   is_ip_locked="false"
+   for iptables_ip in $iptables_rules; do
+      for file_ip in `cat serverdata/$filename`; do
+         if [ "$iptables_ip" == "$file_ip" ]; then
+            is_ip_locked="true"
+         fi
+      done
+   done
+   if [ "$is_ip_locked" == "true" ]; then
+      dialogstring=$dialogstring" on"
+   else
+      dialogstring=$dialogstring" off"
+   fi
 done
 
 result=`dialog --stdout --checklist "Choose servers to block: " 30 40 $counter $dialogstring`
-
 counter=0;
+
+for filename in `ls serverdata`; do
+   for ip in `cat serverdata/$filename`; do
+      sudo iptables -D INPUT -s $ip -j DROP 2> /dev/null
+   done
+done
+
 for filename in `ls serverdata`; do
    (( counter++ ))
    for number in $result; do
